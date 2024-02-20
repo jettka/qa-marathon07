265b2aaf9a26363f94db6ec9f19996b0d3cd1ff0
kobayashi aoi <0921aoi0921@gmail.com>
2024-02-20 11:13:08 +0900

    test.jsとadd-confirmを修正

--- test/cypress/test.cy.js
+++ test/cypress/test.cy.js
@@ -1,5 +1,5 @@
 describe('顧客情報入力フォームのテスト', () => {
-  it('顧客情報を入力して送信し、成功メッセージを確認する', () => {
+  it('顧客情報入力画面から確認画面へ遷移する', () => {
     cy.visit('/aoi_kobayashi/customer/add.html'); // テスト対象のページにアクセス
     cy.window().then((win) => {
       // windowのalertをスタブ化し、エイリアスを設定
@@ -17,13 +17,10 @@ describe('顧客情報入力フォームのテスト', () => {
         cy.get('#contact').type(uniqueContactNumber);
         cy.get('#location').type(data.location);
 
-        
       });
 
    // フォームの送信
      cy.get('#customer-form').submit();
-
-     cy.get('@alertStub').should('have.been.calledOnceWith', '顧客情報が正常に保存されました。');
 
 
 // フォームがリセットされたことを確認
@@ -31,6 +28,23 @@ cy.get('#companyName').should('have.value', '');
 cy.get('#industry').should('have.value', '');
 cy.get('#contact').should('have.value', '');
 cy.get('#location').should('have.value', '');
+
+     cy.wait(3000);
+  });
+
+
+
+  it('確認画面からリストへ追加する', () => {
+    cy.visit('/aoi_kobayashi/customer/add-confirm.html'); // テスト対象のページにアクセス
+
+    cy.wait(1000);
+    cy.get('#add-customer-btn').click();
+    cy.wait(2000);
+   //確認内容がリセットされている確認
+    cy.get('#confirmation-details').should('have.value', '');
+   //入力画面へ戻れるか確認
+   cy.get('#back-btn').click();
+   cy.url().should('include', '/aoi_kobayashi/customer/add.html');
 
      cy.wait(5000);
   });
